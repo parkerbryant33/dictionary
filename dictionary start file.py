cdeaf99e37af457b2bc721c8b7a2853972cc64a2
Parker, Bryant <parker_bryant1@baylor.edu>
2021-02-01 13:12:33 -0600

    initial commit

---
+++ dictionary start file.py
@@ -0,0 +1,113 @@
+phonebook = {"Chris": "555−1111", "Katie": "555−2222", "Joanne": "555−3333"}
+
+print()
+print("*****  start section 1 - print dictionary ********")
+print()
+
+
+print(phonebook)
+
+print()
+print("*****  end section 1 ********")
+print()
+
+
+print()
+print("*****  start section 2 - search dictionary ********")
+print()
+
+if "Chris" in phonebook:
+
+    print(phonebook["Chris"])
+else:
+    print("Chris is not in phonebook")
+
+
+print()
+print("*****  end section 2 ********")
+print()
+
+
+print()
+print("*****  start section 3 - edit/append dictionary ********")
+print()
+
+phonebook["Chris"] = "555-9876"
+phonebook["Joe"] = "999-7878"
+print(phonebook)
+
+
+print()
+print("*****  end section 3 ********")
+print()
+
+
+print()
+print("*****  start section 4 - delete/remove from dictionary ********")
+print()
+
+
+del phonebook["Chris"]
+print(phonebook)
+
+num_items = len(phonebook)
+print(num_items)
+mydict = {}
+print()
+print("*****  end section 4 ********")
+print()
+
+
+print()
+print("*****  start section 5 - iterate through keys ********")
+print()
+
+for k in phonebook:
+    print k
+
+for k in phonebook:
+    print (phonebook[k])
+
+print()
+print("*****  end section 5 ********")
+print()
+
+
+print()
+print("*****  start section 6 - iterate through values  ********")
+print()
+
+for v in phonebook.value():
+    print(v)
+
+print()
+print("*****  end section 6 ********")
+print()
+
+
+print()
+print("*****  start section 7 - iterate through both key and value pair********")
+print()
+
+for pair in phonebook.items()
+    print(pair)
+    print(type(pair))
+
+for k,v
+print()
+print("*****  end section 7 ********")
+print()
+
+
+print()
+print("*****  start section 8 - using random and converting to list ********")
+print()
+
+#clear method deletes all the elements in a dictionary leaving it empty
+#get method gets a value associated with specifed key from dictionary
+#popitem method NOT WORKING returns a randomly selected key value pair and removes that key value pair from the dictionary
+
+
+print()
+print("*****  end section 8 ********")
+print()
